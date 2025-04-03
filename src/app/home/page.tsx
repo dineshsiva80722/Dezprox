1b5d5e5d6e12c1c6dc50b088d25ba22751ad908b
dineshsiva <dineshsiva693@example.com>
2025-04-03 12:29:40 +0530

    home page updated

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>
+    <div className='bg-zinc-500 min-h-screen flex items-center justify-center text-center font-bold '>
       hello
     </div>
   )
