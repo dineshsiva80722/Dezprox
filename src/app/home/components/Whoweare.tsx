5069d1cf2f97367135ce7a40cb3542619c2777e7
dineshsiva <dineshsiva693@example.com>
2025-04-03 14:32:53 +0530

    calendericon updated

--- src/app/home/components/Whoweare.tsx
+++ src/app/home/components/Whoweare.tsx
@@ -80,27 +80,27 @@ const Whoweare: React.FC = () => {
   ];
 
   // Calendar icon component inline
-  const CalendarIcon = () => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" />
-      <path d="M16 3v4" />
-      <path d="M8 3v4" />
-      <path d="M4 11h16" />
-      <path d="M11 15h1" />
-      <path d="M12 15v3" />
-    </svg>
-  );
+  // const CalendarIcon = () => (
+  //   <svg
+  //     xmlns="http://www.w3.org/2000/svg"
+  //     width="20"
+  //     height="20"
+  //     viewBox="0 0 24 24"
+  //     strokeWidth="2"
+  //     stroke="currentColor"
+  //     fill="none"
+  //     strokeLinecap="round"
+  //     strokeLinejoin="round"
+  //   >
+  //     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+  //     <path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" />
+  //     <path d="M16 3v4" />
+  //     <path d="M8 3v4" />
+  //     <path d="M4 11h16" />
+  //     <path d="M11 15h1" />
+  //     <path d="M12 15v3" />
+  //   </svg>
+  // );
 
   // Arrow right icon component inline
   const ArrowRightIcon = () => (
