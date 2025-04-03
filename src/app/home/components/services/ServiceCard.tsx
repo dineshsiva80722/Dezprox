53933419b58635f9af5325ffb46530fce3d54f53
dineshsiva <dineshsiva693@example.com>
2025-04-03 14:34:15 +0530

    servicescard updated

--- src/app/home/components/services/ServiceCard.tsx
+++ src/app/home/components/services/ServiceCard.tsx
@@ -1,7 +1,8 @@
+"use client";
 import React from 'react';
 import { Card } from '@/components/ui/card';
 
-export const ServiceCard = ({ service }) => {
+export const ServiceCard = ({ service }: { service: any }) => {
   const StatsIcon = service.stats.icon;
   
   return (
